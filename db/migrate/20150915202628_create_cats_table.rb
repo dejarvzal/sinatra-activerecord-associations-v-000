00d45dcf1d3d62f6c3dec26d0c3c39ff216bb8fa
Dianne Jarvis-Lumsden <diannejarvisj@gmail.com>
2020-04-26 00:35:30 +0000

    Automatically backed up by Learn

--- db/migrate/20150915202628_create_cats_table.rb
+++ db/migrate/20150915202628_create_cats_table.rb
@@ -1,6 +1,6 @@
 class CreateCatsTable < ActiveRecord::Migration
   def change
-    create_table :create_cats_table do |t|
+    create_table :cats do |t|
       t.string :name
       t.integer :age
       t.string :breed
